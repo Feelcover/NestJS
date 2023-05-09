e906688f8f6b807869fbc5f5b2b1b342722dbb5b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-09 23:47:33 +0300

    перенос функционала в сервисы

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,24 +1,25 @@
 import { Body, Controller, Get, Param, Post} from '@nestjs/common';
 import { ITask } from 'test/task.interface';
+import { TestService } from 'test/test.service';
 
 
 
 @Controller('task')
 export class AppController {
+  constructor (private testService: TestService ) {}
 
  @Get()
  getTasks():ITask[] {
-  return this.tasks;
+  return this.testService.getTasks();
  } 
 
  @Get(':id')
- getTaskById(@Param("id") id:number):ITask {
-  return this.tasks.find(t => t.id === +id);
+ getTaskById(@Param("id") id:string):ITask {
+  return this.testService.getTaskById(id);
  }
 
  @Post()
  createTask(@Body("task") task:ITask):ITask {
-  this.tasks.push(task);
-  return task;
+  return this.testService.createTask(task);
  }
 }
