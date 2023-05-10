84726cfabdd6a384dc7f85a31d6cd89cd2cd06d9
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-10 22:53:25 +0300

    дополнение для инетрфейса

--- src/task/task.interface.ts
+++ src/task/task.interface.ts
@@ -1,4 +1,15 @@
+enum Status {
+  CREATE = 'created',
+  PROCESSING = 'processing',
+  ABORT = 'abort',
+  ERROR = 'error',
+  DONE = 'done',
+}
 export interface ITask {
-    id:number;
-    task: string;
-  }
+  id: number;
+  task: string;
+  status: Status;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
