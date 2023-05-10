031f878e8cb8af161b07db585ba2fe0d54b97365
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-10 23:16:35 +0300

    валидация dto

--- src/task/dto/createTask.dto.ts
+++ src/task/dto/createTask.dto.ts
@@ -1,7 +1,15 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { Status } from '../task.interface';
 
 export class CreateTaskDto {
+  @IsString({ message: 'Укажите название задачи' })
   task: string;
+
+  @IsOptional()
+  @IsString({ each: true, message: 'Неверный тип тегов' })
   tags?: string[];
+
+  @IsOptional()
+  @IsEnum(Status, { message: 'Неверный тип статуса' })
   status?: Status;
 }
